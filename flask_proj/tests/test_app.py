0d8c2002a8c2febe40e480603623cd4766c66adf
AlahariVarsha <alahari.varsha@wipro.com>
2026-06-10 10:14:08 +0530

    Add flask project files

---
+++ flask_proj/tests/test_app.py
@@ -0,0 +1,23 @@
+from app import app
+#from flask_proj.app import app
+
+def test_home():
+
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    assert b"Welcome to Jenkins CI Pipeline" in response.data
+
+
+def test_health():
+
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+    assert b"UP" in response.data
